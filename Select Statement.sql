36b6f3f8712174ba94dfc8238a44c06df8305b6d
Muhammad Abdullah <nisarabdullah6741@gmail.com>
2025-12-20 16:42:02 +0500

    Initial commit

---
+++ Select Statement.sql
@@ -0,0 +1,7 @@
+USE sales;
+
+SELECT * FROM customers; -- Selects all columns from the customers table
+
+SELECT customer_code FROM customers; -- Selects specific column from table customer 
+
+SELECT custmer_name,customer_type FROM customers;
